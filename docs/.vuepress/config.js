a556bddc641443afeb8bec436d3b93e802e50c45
yinjiazeng <yinjiazeng@163.com>
2019-08-04 15:19:38 +0800

    文档首页开发

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  title: "Hello VuePress",
-  description: "Just playing around"
+  title: "NuomiJS",
+  description: "React + Redux + Router最佳实践",
+  head: [
+    ['link', { rel: 'icon', href: '/images/logo.png' }],
+    ['link', { rel: 'stylesheet', href: '/styles/page.css' }],
+  ],
+  themeConfig: {
+    nav: [
+      { text: '指南', link: '/guide/' },
+      { text: 'API', link: '/api/' },
+      { text: '更新日志', link: '/logs/' },
+      { text: 'Github', link: '/logs/' },
+      { text: '招聘', link: '/logs/' },
+    ],
+  },
 };
